61f4fc2f3a403f7347fbbc5cc4d2f6ed2ec08dfe
priana13 <herian.sap@gmail.com>
2024-01-19 11:27:25 +0700

    form page

---
+++ app/Filament/Pages/FormulirPengawasan.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Models\Kategori;
+use App\Models\Proyek;
+use Filament\Pages\Page;
+
+class FormulirPengawasan extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.formulir-pengawasan';
+
+    protected static ?string $navigationLabel = 'Formulir';
+
+    public $list_proyek;
+
+    public $list_kategori;
+
+    public function mount(){
+
+        $this->list_proyek = Proyek::get();
+
+        $this->list_kategori = Kategori::get();
+       
+    }
+
+}
